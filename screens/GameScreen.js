e7c019506c3fb3732ab6dd7b935cec25f38cac1f
Sean Matthews <spmatthews03@gmail.com>
2019-08-09 13:54:17 -0700

    games screen close to complete

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ImageBackground, StyleSheet, View, StatusBar, Image } from 'react-native';
+import { ImageBackground, StyleSheet, View, StatusBar, Text, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class GameScreen extends React.Component {
@@ -12,42 +12,64 @@ export default class GameScreen extends React.Component {
                 style={styles.backgroundImage}
                 source={require('../assets/images/gameBackground.png')}>
                 <StatusBar hidden={true}/>
-                <View style={{flex:5, paddingTop:20, justifyContent:'center', alignItems:'center'}}>
-                    {/* <Image 
-                        style={{flex:1, width:undefined}}
-                        source={require('../assets/images/rimpimpamtournament.png')}/> */}
-                    <Image 
-                        style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
-                        source={require('../assets/images/gameboard.png')}/>
-                </View>
-                <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                    <Button
-                        title="Bet All"
-                        type="outline"
-                        raise={true}
-                        containerStyle={{padding:5}}
-                        buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
-                        titleStyle={{color:'white'}}
-                        // onPress={() => navigate('Game')}
-                        />
-                    <Button
-                        title="Jackpot"
-                        type="outline"
-                        raise={true}
-                        containerStyle={{padding:5}}
-                        buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
-                        titleStyle={{color:'white'}}
-                        // onPress={() => navigate('Game')}
-                        />
-                    <Button
-                        title="Deal"
-                        type="outline"
-                        raise={true}
-                        containerStyle={{padding:5}}
-                        buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
-                        titleStyle={{color:'white'}}
-                        // onPress={() => navigate('Game')}
-                        />
+                <View style={{flex:1, flexDirection:'row'}}> 
+                    <View style={{flex:2}}/>                
+                    <View style={{flex:4}}>
+                        <View style={{flex:1}}>
+                            <Image 
+                                style={{flex:6, width:'100%', height: '100%', resizeMode:'contain'}}
+                                source={require('../assets/images/gameboard.png')}/>
+                            <View style={{flex:1, flexDirection:'row', justifyContent:'center', padding:5}}>
+                                <Image 
+                                    style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
+                                    source={require('../assets/images/gold_chip.png')}/>
+                                <Image 
+                                    style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
+                                    source={require('../assets/images/gold_chip.png')}/>
+                                <Image 
+                                    style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
+                                    source={require('../assets/images/gold_chip.png')}/>
+                                <Image 
+                                    style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
+                                    source={require('../assets/images/gold_chip.png')}/>
+                                <Image 
+                                    style={{flex:1, width:'100%', height: '100%', resizeMode:'contain'}}
+                                    source={require('../assets/images/gold_chip.png')}/>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{flex:2, justifyContent:'space-between', alignItems:'center'}}>
+                        <Text>Jackpot</Text>
+                        <View>
+                            <Button
+                                title="Bet All"
+                                type="outline"
+                                raise={true}
+                                containerStyle={{padding:5}}
+                                buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
+                                titleStyle={{color:'white'}}
+                                // onPress={() => navigate('Game')}
+                                />
+                            <Button
+                                title="Jackpot"
+                                type="outline"
+                                raise={true}
+                                containerStyle={{padding:5}}
+                                buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
+                                titleStyle={{color:'white'}}
+                                // onPress={() => navigate('Game')}
+                                />
+                        </View>
+                        <Button
+                            title="Deal"
+                            type="outline"
+                            raise={true}
+                            containerStyle={{padding:5}}
+                            buttonStyle={{borderRadius:15, borderColor:'white', width:100}}
+                            titleStyle={{color:'white'}}
+                            // onPress={() => navigate('Game')}
+                            />
+                    </View>
                 </View>
             </ImageBackground>
         );
