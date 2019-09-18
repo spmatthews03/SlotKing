c529e82a6e2695f207b15d45aa5a3e38460042e7
Sean Matthews <spmatthews03@gmail.com>
2019-09-18 07:29:27 -0700

    added components

--- components/ScoreBox.js
+++ components/ScoreBox.js
@@ -4,11 +4,20 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'; 
 
 export default class ScoreBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      score_Counter: '00',
+      startDisable: false
+    }
+  }
+
+
   render() {
     return (
         <View style={{flex:1, justifyContent:'center', alignItems:'center', paddingRight:20}}>
             <Text style={{fontSize:12, color:'goldenrod'}}>Score</Text>   
-            <Text style={{fontSize:18, fontWeight:'bold', color:'white'}}>080</Text>      
+            <Text style={{fontSize:18, fontWeight:'bold', color:'white'}}>{this.state.score_Counter}</Text>      
         </View>
     );
   }
---
+++ components/deck.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+ 
+import { StyleSheet, View, Text, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; 
+
+export default class Deck extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timer: null,
+      minutes_Counter: '00',
+      seconds_Counter: '00',
+      startDisable: false
+    }
+  }
+
+  timerStart = () => {
+    let timer = setInterval(() => {
+ 
+      var num = (Number(this.state.seconds_Counter) + 1).toString(),
+        count = this.state.minutes_Counter;
+ 
+      if (Number(this.state.seconds_Counter) == 59) {
+        count = (Number(this.state.minutes_Counter) + 1).toString();
+        num = '00';
+      }
+ 
+      this.setState({
+        minutes_Counter: count.length == 1 ? '0' + count : count,
+        seconds_Counter: num.length == 1 ? '0' + num : num
+      });
+    }, 1000);
+    this.setState({ timer });
+   }
+
+  componentDidMount(){
+    this.timerStart();
+  }
+
+
+  render() {
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text style={styles.timeTitle}>Time</Text>   
+            <Text style={styles.timerText}>{this.state.minutes_Counter}:{this.state.seconds_Counter}</Text>   
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  timerText:{
+    fontSize: 18,
+    fontWeight:'bold',
+    color:'white'
+  },
+  timeTitle:{
+    fontSize:12,
+    color:'goldenrod'
+  }
+})
